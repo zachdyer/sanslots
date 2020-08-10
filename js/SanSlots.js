6568b6f8b05c594fb084d9e26577d2c085b71e3d
zach.dyer <zach.dyer@gmail.com>
2020-08-10 03:09:05 -0400

    first version

---
+++ js/SanSlots.js
@@ -0,0 +1,69 @@
+var slotMachine = new SlotMachine()
+var vm = new Vue({
+  el: '#sanslots',
+  data: {
+    slots: [
+      [{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'},{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'},{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'}],
+      [{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'},{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'},{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'}],
+      [{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'},{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'},{"cardClass": null, type: '', money: null, img: 'url(img/slots/1.jpg)'}]
+    ],
+    slotImages: [
+      new Image(),
+      new Image(),
+      new Image(),
+      new Image()
+    ],
+    slotAnimation: null,
+    alert: {
+      variant: null,
+      message: null
+    }
+  },
+  created(){
+    //preload images
+    // Set the image sources and make sure to label them in numeric order
+    for(var i = 0; i < this.slotImages.length; i++) {
+      this.slotImages[i].src = `img/slots/${i+1}.jpg`
+    }
+    //preload slot animation
+    this.slotAnimation = new Image().src = 'img/loading.3.gif'
+    console.log(this.slots)
+  },
+  methods: {
+    play(){
+      // Calculate new slot values
+      slotMachine.play()
+      // Reset slots to loading
+      for(let x = 0; x < slotMachine.slots.length; x++){
+       for(let y = 0; y < slotMachine.slots[x].length; y++){
+        this.slots[x][y].img = `url(img/loading.3.gif)`
+        this.slots[x][y].type = this.slots[x][y].money = this.slots[x][y].cardClass = ''
+       }
+      }
+      console.log(this.slots)
+      // Reset alert box to null
+      this.alert.variant = null
+      this.alert.message = null
+      // Wait a couple seconds to show slots
+      window.setTimeout(()=>{
+        for(let x = 0; x < slotMachine.slots.length; x++){
+         for(let y = 0; y < slotMachine.slots[x].length; y++){
+           let slotVal = slotMachine.slots[x][y]
+           this.slots[x][y].img = `url(${this.slotImages[slotVal - 1].src})`
+           this.slots[x][y].type = slotVal
+           this.slots[x][y].money = `$${slotVal}0`
+           this.slots[x][y].cardClass =  slotMachine.winnerSlots[x][y] ?  'win-gradient' : ''
+           // If won then update the alert box
+           if(slotMachine.winnings) {
+            this.alert.message = `You win $${slotMachine.winnings}!`
+            this.alert.variant = 'alert-success'
+           }
+         } 
+        }
+      }, 2000)
+    }
+  }
+})
+
+// Debug
+
---
+++ js/SlotMachine.js
@@ -0,0 +1,57 @@
+function SlotMachine(rows = 3, columns = 3){
+	this.winnings = 0
+	this.highscore = 0
+	this.totalKinds = 4
+	this.bid = 10
+	this.slots = [
+		[0,0,0],
+		[0,0,0],
+		[0,0,0]
+	]
+	this.winnerSlots = []
+	this.play = function(){
+		// randomize slots
+		for(let x = 0; x < rows; x++){
+			for(let y = 0; y < columns; y++){
+				let slot = this.slots[x][y]
+				this.slots[x][y] = Math.floor(Math.random() * this.totalKinds) + 1
+			}
+		}
+		// Calculate top middle bottom row wins
+		this.winnings = 0
+		// Track winning slots and reset it every play
+		this.winnerSlots = [
+			[false, false, false],
+			[false, false, false],
+			[false, false, false]
+		]
+		for(let i = 0;  i < rows; i++) {
+			let row = this.slots[i]
+			if(row[0] === row[1] && row[1] === row[2]){ 
+				console.log('horizontal win', row[0]); 
+				this.winnings += row[0]
+				this.winnerSlots[i][0] = this.winnerSlots[i][1] = this.winnerSlots[i][2] = true
+			}
+		}
+		// Check diagonal wins
+		if(this.slots[0][0] === this.slots[1][1] && this.slots[1][1]=== this.slots[2][2]) 
+			{
+				console.log('diagonal win', this.slots[0][0]); 
+				this.winnings += this.slots[1][1]
+				this.winnerSlots[0][0] = true
+				this.winnerSlots[1][1] = true
+				this.winnerSlots[2][2] = true
+			}
+		if(this.slots[2][0] === this.slots[1][1] && this.slots[1][1] === this.slots[0][2]) 
+			{
+				console.log('diagonal win', this.slots[2][0]);
+				this.winnings += this.slots[1][1]
+				this.winnerSlots[2][0] = true
+				this.winnerSlots[1][1] = true
+				this.winnerSlots[0][2] = true
+			}
+		this.winnings *= this.bid * 3
+		if(this.winnings > this.highscore) this.highscore = this.winnings
+		return this.winnings
+	}
+}
